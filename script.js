8c8441ddb488990b75f91e36994f412599eaf915
Radu Potop <wooptoo@gmail.com>
2012-01-17 17:33:50 +0200

    added parseHref

--- script.js
+++ script.js
@@ -1,30 +1,59 @@
+rules = {
+    0: {
+        'match': 'youtube\.com',
+        'search': 'v=',
+        'lenID': 11,
+        'out': 'http://www.youtube.com/embed/',
+    },
+}
+
+
+
+
 function embed() {
 
     len=document.links.length
     for(i=0; i<len; i++) {
         a=document.links[i]
-        if (a.href && a.href.match(/youtube\.com/)) {
 
-            hrefNew = a.href.replace('watch?v=', 'embed/')
-            pos = hrefNew.search('&')
-            if (pos > -1) { hrefNew = hrefNew.slice(0,pos) }
-
-            iframe = createIframe(hrefNew)
-
-            a.appendChild(iframe)
-            //~console.log(a.parentNode)
-        }
+        iframe = createIframe(parseHref(a.href))
+        a.appendChild(iframe)
     }
 
 }
+
+
+function parseHref(inp) {
+
+    if (inp.match(/youtube\.com/)) {
+        pos=inp.search('v=')
+        id=inp.slice(pos+2,pos+13)
+        out='http://www.youtube.com/embed/'+id
+    } else
+    if (inp.match(/youtu\.be/)) {
+        pos=inp.search('.be/')
+        id=inp.slice(pos+4,pos+15)
+        out='http://www.youtube.com/embed/'+id
+    } else
+    if (inp.match(/vimeo\.com/)) {
+        pos=inp.search('.com/')
+        id=inp.slice(pos+5,pos+16)
+        out='http://player.vimeo.com/video/'+id
+    } else {
+        out=null
+    }
+
+    return out
+}
+
 
 function createIframe(src) {
     iframe = document.createElement('iframe')
 
     iframe.src = src
     iframe.setAttribute('allowfullscreen', true)
-    iframe.setAttribute('width', 420)
-    iframe.setAttribute('height', 315)
+    iframe.setAttribute('width', 100)
+    iframe.setAttribute('height', 80)
 
     return iframe
 }

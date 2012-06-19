1e05499778a6ed9ec83171356f73cacb4faa4734
Radu Potop <radu.potop@wooptoo.com>
2012-06-19 23:22:27 +0300

    validate with jshint, fix regex bug

--- embedder.js
+++ embedder.js
@@ -4,7 +4,7 @@
  * A small JS video embedder.
  * It can embed almost any video according to rules.
  *
- * Author: Radu Potop <wooptoo@gmail.com>
+ * Author: Radu Potop <radu.potop@wooptoo.com>
  * License: BSD
  *
 
@@ -19,45 +19,45 @@
 */
 
 
-rules = [
+var rules = [
     {
-        'pattern': 'youtube\.com',
+        'pattern': 'youtube\\.com',
         'search': 'v=',
         'lenID': 11,
-        'url': 'http://www.youtube.com/embed/{ID}',
+        'url': 'http://www.youtube.com/embed/{ID}'
     },
     {
-        'pattern': 'youtu\.be',
+        'pattern': 'youtu\\.be',
         'search': '.be/',
         'lenID': 11,
-        'url': 'http://www.youtube.com/embed/{ID}',
+        'url': 'http://www.youtube.com/embed/{ID}'
     },
     {
-        'pattern': 'vimeo\.com',
+        'pattern': 'vimeo\\.com',
         'search': '.com/',
         'lenID': 8,
-        'url': 'http://player.vimeo.com/video/{ID}',
-    },
-]
+        'url': 'http://player.vimeo.com/video/{ID}'
+    }
+];
 
 
 /*
  * auto start the script
  */
-embedder()
+embedder();
 
 
 function embedder() {
 
-    var len=document.links.length
+    var len=document.links.length;
 
     for(var i=0; i<len; i++) {
-        var a=document.links[i]
-        var src=parseHref(a.href)
+        var a=document.links[i];
+        var src=parseHref(a.href);
 
         if(src) {
-            var iframe = createIframe(src)
-            insertAfter(iframe,a)
+            var iframe = createIframe(src);
+            insertAfter(iframe,a);
         }
     }
 
@@ -66,42 +66,42 @@ function embedder() {
 
 function parseHref(href) {
 
-    var len=rules.length
+    var len=rules.length;
+    var out;
 
     for (var i=0; i<len; i++) {
-        var rule = rules[i]
-        var pattern = new RegExp(rule.pattern, 'i')
+        var rule = rules[i];
+        var pattern = new RegExp(rule.pattern, 'i');
         if (href.match(pattern)) {
-            var pos = href.search(rule.search)
+            var pos = href.search(rule.search);
             if(pos > -1) {
-                pos += rule.search.length
-                var id = href.slice(pos,pos+rule.lenID)
-                var out = rule.url.replace('{ID}',id)
+                pos += rule.search.length;
+                var id = href.slice(pos,pos+rule.lenID);
+                out = rule.url.replace('{ID}',id);
             }
         }
     }
 
-    return out
+    return out;
 }
 
 
 function createIframe(src) {
 
-    var iframe = document.createElement('iframe')
+    var iframe = document.createElement('iframe');
 
-    iframe.src = src
-    iframe.style.width = '500px'
-    iframe.style.height = '375px'
-    iframe.style.borderWidth = '0px'
-    iframe.style.display = 'block'
+    iframe.src = src;
+    iframe.style.width = '500px';
+    iframe.style.height = '375px';
+    iframe.style.borderWidth = '0px';
+    iframe.style.display = 'block';
 
-    return iframe
+    return iframe;
 }
 
 
-/*
- * Credits to: http://blog.svidgen.com/2007/10/javascript-insertafter.html
- * */
+
+// Credits to: http://blog.svidgen.com/2007/10/javascript-insertafter.html
 function insertAfter(new_node, existing_node) {
     if (existing_node.nextSibling) {
         existing_node.parentNode.insertBefore(new_node, existing_node.nextSibling);

de4526d5cc14ac18e25750d0eeb757f3fcf778cf
Radu Potop <radu.potop@wooptoo.com>
2013-11-11 22:43:26 +0200

    added youtube playlist rule

--- embedder.js
+++ embedder.js
@@ -23,6 +23,10 @@
  */
 var rules = [
     {
+        'pattern': /.*youtube\.com\/.*v=([\w\-]{11}).*list=([\w\-]{34}|[\w\-]{18}).*/i,
+        'url': 'http://www.youtube.com/embed/$1?list=$2'
+    },
+    {
         'pattern': /.*youtube\.com\/.*v=([\w\-]{11}).*/i,
         'url': 'http://www.youtube.com/embed/$1'
     },
